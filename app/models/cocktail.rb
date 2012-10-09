fdaaa8332a4493bb8f0eee039aaa234869cae102
Christine McClure <camcclure@gmail.com>
2012-10-09 14:27:58 -0500

    added method to save cocktail name to lowercase

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,4 +2,9 @@ class Cocktail < ActiveRecord::Base
   attr_accessible :name, :notes
   validates :name,  presence: true
   has_many :ingredients, dependent: :destroy
+
+ def name=(val)
+    write_attribute(:name, val.downcase)
+ end
+
 end
