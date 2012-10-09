a3d3fc3fb1588e4141735d6dcad9aa1184359deb
Christine McClure <camcclure@gmail.com>
2012-10-09 14:53:17 -0500

    updated seed data to be more realistic

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,19 +9,50 @@
 Cocktail.delete_all
 Ingredient.delete_all
 
-cocktail = Cocktail.create(:name => "Sazerac", :notes => "Mix all")
+cocktail = Cocktail.create(:name => "Sazerac",
+  :notes => "Chill an old-fashioned glass by filling it with ice and letting it sit while preparing the rest of the drink.
+In a separate mixing glass, muddle the simple syrup and Peychaud bitters together.
+Add the rye whiskey and ice to the bitters mixture and stir.
+Discard the ice in the chilled glass and rinse it with absinthe (or substitute) by pouring a small amount into the glass, swirling it around and discarding the liquid.
+Strain the whiskey mixture from the mixing glass into the old fashioned glass.
+Garnish with a lemon twist. Traditionalists will say that the lemon twist should be squeezed over the drink to release its essences but that the twist should not be dropped into the glass itself."
+)
 ingredients = cocktail.ingredients.create([
   {:ingredient => "3 oz rye whiskey"},
   {:ingredient => "3/4 oz simple syrup"},
-  {:ingredient => "spash absinthe"}
+  {:ingredient => "spash absinthe"},
+  {:ingredient => "Peychaud bitters to taste"},
+  {:ingredient => "lemon twist for garnish"}
 ])
 
-cocktail = Cocktail.create(:name => "Singapore Sling", :notes => "Mix all")
+cocktail = Cocktail.create(:name => "Singapore Sling",
+  :notes => "Pour the gin, lemon juice, sugar syrup and powdered sugar into a shaker with ice cubes.
+Shake well.
+Strain into a highball glass with ice cubes.
+Pour in the club soda.
+Float the cherry brandy on top by pouring it over the back of a bar spoon.
+Garnish with the lemon slice and cherry."
+)
 ingredients = cocktail.ingredients.create([
-  {:ingredient => "1/2 oz grenadine"},
-  {:ingredient => "1 oz gin"},
-  {:ingredient => "club soda"}
+  {:ingredient => "1 1/2 oz gin"},
+  {:ingredient => "1 oz lemon juice"},
+  {:ingredient => "1/4 oz sugar syrup"},
+  {:ingredient => "1 1/2 tsp powdered sugar"},
+  {:ingredient => "2 oz club soda"},
+  {:ingredient => "1/2 oz cherry brandy, kirsch, or Cherry Heering"},
+  {:ingredient => "lemon slice for garnish"},
+  {:ingredient => "maraschino cherry for garnish"}
 ])
 
-cocktail = Cocktail.create(:name => "Manhattan", :notes => "Mix all")
+cocktail = Cocktail.create(:name => "Manhattan", 
+  :notes => "Pour the ingredients into a mixing glass with ice cubes.
+Stir well.
+Strain into a chilled cocktail glass.
+Garnish with the cherry.")
+ingredients = cocktail.ingredients.create([
+  {:ingredient => "2 oz rye whiskey"},
+  {:ingredient => "1/2 oz sweet vermouth"},
+  {:ingredient => "2-3 dashes Angostura bitters"},
+  {:ingredient => "maraschino cherry for garnish"}
+  ])
 
