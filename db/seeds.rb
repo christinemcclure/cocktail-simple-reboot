d1ed103ff34fc2f533f11cc96e63c327048ac219
Christine McClure <camcclure@gmail.com>
2012-10-10 17:15:22 -0500

    added more test data, probably because I'd like a cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -58,10 +58,10 @@ ingredients = cocktail.ingredients.create([
 
 cocktail = Cocktail.create(:name => "Bellini",
   :notes => "Pour pureed white peaches and lemon juice in bottom of a champagne flute.
-            Carefully top off with Prosecco.
-            Stir gently.
-            Garnish with peach slice.
-            A 1/4 ounce of peach schnapps may be added to the pureed white peaches to give this cocktail added punch.")
+Carefully top off with Prosecco.
+Stir gently.
+Garnish with peach slice.
+A 1/4 ounce of peach schnapps may be added to the pureed white peaches to give this cocktail added punch.")
 ingredients = cocktail.ingredients.create([
   {:ingredient => "1 ounce Pureed White Peaches or peach nectar"},
   {:ingredient => "1/4 teaspoon Freshly Squeezed Lemon Juice"},
@@ -71,16 +71,44 @@ ingredients = cocktail.ingredients.create([
 
 cocktail = Cocktail.create(:name => "Caipirinha",
   :notes => "Place lime wedges in cocktail shaker, reserving one for garnish.
-            Muddle.
-            Pour Cachaca and bar syrup into cocktail shaker and fill two-thirds with ice.
-            Shake to combine well.
-            Place ice into old-fashioned glass--Do not overfill.
-            Strain contents of shaker into glass.
-            Garnish with one reserved lime wedge.")
+Muddle.
+Pour Cachaca and bar syrup into cocktail shaker and fill two-thirds with ice.
+Shake to combine well.
+Place ice into old-fashioned glass--Do not overfill.
+Strain contents of shaker into glass.
+Garnish with one reserved lime wedge.")
 ingredients = cocktail.ingredients.create([
   {:ingredient => "2 ounces Cachaca"},
   {:ingredient => "1 lime cut into wedges"},
   {:ingredient => "1/2 ounce simple syrup"}
 ])
 
+cocktail = Cocktail.create(:name => "tom collins",
+  :notes => "Stir all of the above together in the Tom Collins glass.
+Add 4 ice-cubes.
+Fill with soda water, stir again, and serve immediately.")
+ingredients = cocktail.ingredients.create([
+  {:ingredient => "1 tablespoon sugar syrup"},
+  {:ingredient => "Juice of 1 medium lemon"},
+  {:ingredient => "3-4 ounces of gin (2 measures)"}
+])
 
+
+cocktail = Cocktail.create(:name => "mint julep",
+  :notes => "In a tall bar glass put syrup, mint, and 2 or 3 dashes of angostura bitters.
+Bruise the mint gently with a muddler and blend the whole mix by stirring and pressing slightly (do NOT use a blender). 
+Do not crush the mint leaves or it will release bitter juices. 
+Pour bourbon over the top.
+Remove julep glasses from the refrigerator and fill with crushed ice (being careful not to touch the glasses with your bare hands). 
+Pour the julep over the top and stir for a few minutes. 
+Add some more ice and top with enough Bourbon to fill the glass to 1/4 inch from the top. 
+Insert two long straws and decorate with sugared mint leaves and serve.")
+
+ingredients = cocktail.ingredients.create([
+  {:ingredient => "1 Tbsp simple syrup"},
+  {:ingredient => "a dozen young mint leaves"},
+  {:ingredient => "1 tsp powdered sugar"},
+  {:ingredient => "2 tsp water"},
+  {:ingredient => "angostura bitters"},
+  {:ingredient => "2 ounces Bourbon"}
+])
