ff159fca518aa9db2ad60f56049dd3d4be6ea01a
Christine McClure <camcclure@gmail.com>
2012-10-10 17:01:02 -0500

    added default sort by name in cocktail model

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,7 +2,7 @@ class Cocktail < ActiveRecord::Base
   attr_accessible :name, :notes
   validates :name,  presence: true
   has_many :ingredients, dependent: :destroy
-
+  default_scope :order => 'cocktails.name'
  def name=(val)
     write_attribute(:name, val.downcase)
  end
